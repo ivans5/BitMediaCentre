5f2633da118a5737646ec7d83fab381106c7c641
Your Name <you@example.com>
2020-05-05 16:13:45 -0700

    -docker starts an network interface by default on fedora 32 now?

--- rc-server/rc-server.py
+++ rc-server/rc-server.py
@@ -1,11 +1,22 @@
 from evdev import UInput, ecodes as e
-import netifaces, socket
+import netifaces, socket, subprocess
 
 #Determine broadcast IP to use:
-for x in netifaces.interfaces():
-  y=netifaces.ifaddresses(x)
-  if socket.AF_INET in y and 'broadcast' in y[socket.AF_INET][0]:
-    broadcast_ip = y[socket.AF_INET][0]['broadcast']
+#Prefer the first result from 'nmcli' command:
+preferred_iface=None
+try: 
+  preferred_iface=subprocess.check_output("nmcli d show |grep GENERAL.DEVICE|head -n+1|awk '{print $2}'",shell=True).decode().rstrip()
+except:
+  pass 
+
+if preferred_iface == None:
+  for x in netifaces.interfaces():
+    y=netifaces.ifaddresses(x)
+    print (x+": "+str(y))
+    if socket.AF_INET in y and 'broadcast' in y[socket.AF_INET][0]:
+      broadcast_ip = y[socket.AF_INET][0]['broadcast']
+else:
+  broadcast_ip = netifaces.ifaddresses(preferred_iface)[socket.AF_INET][0]['broadcast']
 
 UDP_PORT = 10000
 
