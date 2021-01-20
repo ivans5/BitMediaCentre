76e27948287d274b8f61572650c940edd2a0f9ea
Your Name <you@example.com>
2021-01-20 07:01:06 -0800

    -replace aria2 with transmission 3

---
+++ cloud-server/mytransmission/download_wrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+KEY_SPEC=$(echo -n $ENCRYPTED_KEY_SPEC|base64 --decode|openssl rsautl -decrypt -inkey /homepcuser/private_key.pem -raw)
+IV_SPEC=$(echo -n $ENCRYPTED_IV_SPEC|base64 --decode|openssl rsautl -decrypt -inkey /homepcuser/private_key.pem -raw)
+PAYLOAD=$(echo -n $ENCRYPTED_PAYLOAD|base64 --decode|openssl aes-128-cbc -nosalt -K $KEY_SPEC -iv $IV_SPEC -d)
+
+echo STARTING
+
+cd /homepcuser && exec transmission-remote _gateway -a "$PAYLOAD" #--seed-time=0
+
