e002c56a5d183c396448eb589a0bab83119a31f4
Your Name <you@example.com>
2021-05-21 18:43:37 -0700

    upgrade to f34 - fix pulseaudio

---
+++ bitmediacentre-base-rpm/youtube-dl/ck2-china.sh
@@ -0,0 +1,1 @@
+mpv --title=mpvFullscreen -fs https://www.youtube.com/watch?v=ZX2uXBMkO8U
---
+++ bitmediacentre-base-rpm/youtube-dl/trt.sh
@@ -0,0 +1,1 @@
+mpv --title=mpvFullscreen -fs $(/usr/local/bin/youtube-dl -f best -g "ytsearch1:trtworld live")
