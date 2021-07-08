8103e6ab4aaa61e5efe521de142b5915e021077b
Your Name <you@example.com>
2021-07-08 06:35:27 -0700

    factor out start script into seperate file 2

---
+++ bitmediacentre-base-rpm/bitmediacentre-start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+[ -e /env.sh ] || ln -s /usr/start/etc/env.sh /env.sh
+
+[ -e /start ] || { mkdir -p /var/start/; ln -s /usr/start/youtube /var/start/youtube; ln -s /var/start /start; ln -s /var/home/pcuser /homepcuser; }
+
+
+#Note: according https://www.freedesktop.org/wiki/Software/PulseAudio/FAQ/
+#XDG_RUNTIME_DIR is supposed to be on ramdisk!
+
+rm -rf /tmp/xdg_runtime_dir
+mkdir -p /tmp/xdg_runtime_dir
+chown pcuser:pcuser /tmp/xdg_runtime_dir
+chmod 700 /tmp/xdg_runtime_dir
+chcon -u system_u -t user_tmp_t /tmp/xdg_runtime_dir
+
+exit 0
